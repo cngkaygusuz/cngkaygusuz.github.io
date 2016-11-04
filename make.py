91c2add904cffad90434dff7511682a3a919efc0
ckaygusu <cemkyg@gmail.com>
2016-11-04 18:09:01 +0200

    Jinja2 trial

--- make.py
+++ make.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python
 import os
 
-
-def _exec():
-    pass
+import jinja2
 
 
 def render():
-    pass
+    """Render templates."""
+    # read from src, render, copy to build, within the same path
+    loader = jinja2.FileSystemLoader(['src/', 'templates/'])
+    env = jinja2.Environment(loader=loader)
+
+
+    template = env.get_template("index.html")
+    print(template.render())
 
 
 def venv():
+    """Set up local env for development."""
     os.system("sudo apt-get -y install virtualenv")
     os.system("virtualenv --no-site-packages -p /usr/bin/python3 venv/")
+    os.system("PATH=venv/bin; pip install -r requirements.txt")
 
-venv()
+render()
